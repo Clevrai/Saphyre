5b0e8d70f33194f0919a00cc81b2e46c021a2cc8
clevr <72430968+clevr-Technologies@users.noreply.github.com>
2021-07-14 23:32:48 -0600

    Create pre_process_image.py

---
+++ saphyre/pre_process_image.py
@@ -0,0 +1,128 @@
+import numpy as np
+import cv2
+from google.colab.patches import cv2_imshow
+from PIL import Image
+from PIL import ImageEnhance
+from PIL import ImageFilter
+from PIL import ImageStat
+import statistics as stats
+import math
+
+'''helper'''
+
+kelvin_table = {
+    1000: (255,56,0),
+    1500: (255,109,0),
+    2000: (255,137,18),
+    2500: (255,161,72),
+    3000: (255,180,107),
+    3500: (255,196,137),
+    4000: (255,209,163),
+    4500: (255,219,186),
+    5000: (255,228,206),
+    5500: (255,236,224),
+    6000: (255,243,239),
+    6500: (255,249,253),
+    7000: (245,243,255),
+    7500: (235,238,255),
+    8000: (227,233,255),
+    8500: (220,229,255),
+    9000: (214,225,255),
+    9500: (208,222,255),
+    10000: (204,219,255)}
+
+
+def convert_temp(image, temp):
+    r, g, b = kelvin_table[temp]
+    matrix = ( r / 255.0, 0.0, 0.0, 0.0,
+               0.0, g / 255.0, 0.0, 0.0,
+               0.0, 0.0, b / 255.0, 0.0 )
+    return image.convert('RGB', matrix)
+
+
+
+def getAverageRGB(image):
+    npixels = image.size[0]*image.size[1]
+    cols = image.getcolors(npixels)
+    sumRGB = [(x[0]*x[1][0], x[0]*x[1][1], x[0]*x[1][2]) for x in cols] 
+    avg = tuple([sum(x)/npixels for x in zip(*sumRGB)])
+    return avg, stats.mean(avg)
+
+
+
+def variance_of_laplacian(image):
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+
+
+def rgb_to_hsv(r, g, b):
+    r, g, b = r/255.0, g/255.0, b/255.0
+    mx = max(r, g, b)
+    mn = min(r, g, b)
+    df = mx-mn
+    if mx == mn:
+        h = 0
+    elif mx == r:
+        h = (60 * ((g-b)/df) + 360) % 360
+    elif mx == g:
+        h = (60 * ((b-r)/df) + 120) % 360
+    elif mx == b:
+        h = (60 * ((r-g)/df) + 240) % 360
+    if mx == 0:
+        s = 0
+    else:
+        s = (df/mx)*100
+    v = mx*100
+    return h, s, v
+
+
+def brightness( im_file ): 
+    im = Image.open(im_file).convert('L')
+    stat = ImageStat.Stat(im) 
+    return stat.mean[0]
+
+
+def saturation(image):
+    img = Image.open(image)
+    rgb = getAverageRGB(img)
+    if rgb[0][0] < rgb[0][2]:
+        return [rgb, 'Increase']
+    return rgb
+
+
+
+def sharpness_level(image):
+    image = cv2.imread(image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    fm = variance_of_laplacian(gray)
+    text = "Not Blurry"
+
+    if fm < 100:
+      text = "Blurry"
+    
+    return (text,fm)
+
+
+def adjust_gamma(image, gamma=1.0):
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+      for i in np.arange(0, 256)]).astype("uint8")
+    return cv2.LUT(image, table)
+
+
+
+def white_balance_loops(img):
+    result = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    avg_a = np.average(result[:, :, 1])
+    avg_b = np.average(result[:, :, 2])
+    for x in range(result.shape[0]):
+        for y in range(result.shape[1]):
+            l, a, b = result[x, y, :]
+            # fix for CV correction
+            l *= 100 / 255.0
+            result[x, y, 1] = a - ((avg_a - 128) * (l / 100.0) * 1.1)
+            result[x, y, 2] = b - ((avg_b - 128) * (l / 100.0) * 1.1)
+    result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
+    return result
+
+
+
